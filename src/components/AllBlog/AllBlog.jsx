9183604fb786d236a45a27872858b134725c1875
Md.Mohiuddin <saddammd203@gmail.com>
2023-04-01 01:08:39 +0600

    added blogs img and title

--- src/components/AllBlog/AllBlog.jsx
+++ src/components/AllBlog/AllBlog.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './AllBlog.css'
 const Product = (props) => {
-   const {coverImg,name} =props.blogItem;
+   const {coverImg,name,authorImg,blogTitle,duration} =props.blogItem;
     return (
         <div>
-        <img src={coverImg} alt="" />
+        <img className='cover-img' src={coverImg} alt="" />
+        <img className='author-img' src={authorImg} alt="" />
+        <h3>{name}</h3>
+        <h5>Mar 14 (4 Days ago)</h5>
+        <h1>{blogTitle}</h1>
+        <h5>{duration}</h5>
         </div>
     );
 };
