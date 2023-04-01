5a72c29cb22095fa56ed329172c558f66f9f34fc
Md.Mohiuddin <saddammd203@gmail.com>
2023-04-01 11:08:59 +0600

    added blog styles

--- src/components/AllBlog/AllBlog.jsx
+++ src/components/AllBlog/AllBlog.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import './AllBlog.css'
 const Product = (props) => {
-   const {coverImg,name,authorImg,blogTitle,duration} =props.blogItem;
+    const { coverImg, name, authorImg, blogTitle, duration, markAsRead } = props.blogItem;
     return (
-        <div>
-        <img className='cover-img' src={coverImg} alt="" />
-        <img className='author-img' src={authorImg} alt="" />
-        <h3>{name}</h3>
-        <h5>Mar 14 (4 Days ago)</h5>
-        <h1>{blogTitle}</h1>
-        <h5>{duration}</h5>
+        <div className='blogDetails'>
+            <img className='cover-img' src={coverImg} alt="" />
+            <div className='nameDetails'>
+                <img className='author-img' src={authorImg} alt="" />
+               
+               <div className='nameDate'>
+               <h3 className='name'>{name}</h3>
+                <h5>Mar 14 (4 Days ago)</h5>
+               </div>
+                <h5 className='duration'>{duration}</h5>
+               </div>
+
+            <h1>{blogTitle}</h1>
+            <h4>{markAsRead}</h4>
+
         </div>
     );
 };
